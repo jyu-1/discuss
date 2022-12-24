5d06e66e52a28ffb2d9606626406add428a7adfd
jyu-1 <114373319+jyu-1@users.noreply.github.com>
2022-12-24 09:11:53 -0800

    remove boilerplates and change defaults

---
+++ src/Routing.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Routing = () => {
+    return (
+        <BrowserRouter basename="discuss">
+            <Routes></Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Routing;
